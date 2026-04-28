febeed954d792a345c6baa6dc0bc775e9aeae465
Peter CODE <pet3r.code@gmail.com>
2026-04-28 07:46:23 +0300

    Command Line: Bash/Terminal Added: ffmpeg - Tools for transcoding, streaming and playing of multimedia files Modified: script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@ sudo apt-get install python3-pip
 sudo apt-get install python3-tk
 sudo apt-get install rar
 sudo apt-get install unrar
+sudo apt-get install ffmpeg
 
 ### Accessories ###
 sudo apt-get install pluma
